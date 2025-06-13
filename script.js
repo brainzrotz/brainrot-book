948a11fa73d20a8a9bd0ca723f77add08de1f973
Cristjan <cristjan@gmail.com>
2025-06-13 19:54:11 +0300

    jpsg

--- script.js
+++ script.js
@@ -4,163 +4,163 @@ const audio = new Audio("click.mp3");
 // 📖 Full scene list
 const scenes = [
   {
-    img:  "lobster1.jpg",
+    img:  "JPG/lobster1.jpg",
     text: "Lobsteroni Supreme",
     subtitle: "Appears at 3AM demanding a dance battle... and orange juice.",
     choices: ["Join in!", "I love to dance"]
   },
   {
-    img: "lobster2.jpg",
+    img: "JPG/lobster2.jpg",
     text: "Giuseppe DunDum Snapolino",
     subtitle: "Claims to know your name. Probably learned it from a ravioli.",
     choices: ["Shout your name!", "You don't know me bro"]
   },
   {
-    img: "lobster3.jpg",
+    img: "JPG/lobster3.jpg",
     text: "Lobstuseni Fliporeni",
     subtitle: "Dudum dish...",
     choices: ["Skate in sauce", "Call mom with noodle hands"]
   },
   {
-    img: "milkcarton.jpg",
+    img: "JPG/milkcarton.jpg",
     text: "Milkerini Drinkenini",
     subtitle: "The god of breakfast...",
     choices: ["Drink it", "Swim in milk"]
   },
   {
-    img: "bed.jpg",
+    img: "JPG/bed.jpg",
     text: "Beddy McSleepface",
     subtitle: "Pillow has eyes. Blanket has opinions.",
     choices: ["Sleep on it", "Talk to it"]
   },
   {
-    img: "scene1.jpg",
+    img: "JPG/scene1.jpg",
     text: "Whisperooni del Wallissimo",
     subtitle: "You can’t see him, but he’s definitely chewing pasta in there.",
     choices: ["Listen closer", "Prefer milk"]
   },
   {
-    img: "scene2.jpg",
+    img: "JPG/scene2.jpg",
     text: "Blurrito Scootson",
     subtitle: "Blink once: he's gone. Blink twice: you're a spaghettizini",
     choices: ["Hop on", "Hop on"]
   },
   {
-    img: "scene3.jpg",
+    img: "JPG/scene3.jpg",
     text: "Detective Cheez-Hawk",
     subtitle: "Arrested a lasagna for being ‘too mysterious’. You’re next.",
     choices: ["Apologize to the pizza", "Accept the new reality"]
   },
   {
-    img: "giraffe.jpg",
+    img: "JPG/giraffe.jpg",
     text: "Meet Turbo Toastneck",
     subtitle: "He honks his way through candy chaos.",
     choices: ["Honk back", "Nice car"]
   },
   {
-    img: "mrspaghetti.jpg",
+    img: "JPG/mrspaghetti.jpg",
     text: "Spaghettino Mozzarella",
     subtitle: "Complete with meatball aura and floating cheese drama.",
     choices: ["Love meatballs", "More cheese!!"]
   },
   {
-    img: "waffle.jpg",
+    img: "JPG/waffle.jpg",
     text: "DJ Croak-Waffle 🐸",
     subtitle: "Frog king of brain rot breakfast city!",
     choices: ["Eggy Yolkface", "Frooooog"]
   },
   {
-    img: "hamster.jpg",
+    img: "hJPG/amster.jpg",
     text: "Skatemo Puffzoom 🐹",
     subtitle: "Fueled by neon chaos and unlicensed energy drinks.",
     choices: ["Pump up the chaos", "Catch him if you can"]
   },
   {
-    img: "kroko.jpg",
+    img: "JPG/kroko.jpg",
     text: "Floaty McZoomjaw 🐊",
     subtitle: "Cereal-fueled panic energy!",
     choices: ["Chomp & Dash", "Wave back at Banana"]
   },
   {
-    img: "lama.jpg",
+    img: "JPG/lama.jpg",
     text: "Llamagician Crumbspark",
     subtitle: "Levitating with a glowing sandwich",
     choices: ["Cast Sandwich Spell", "Duel the Popcorn Machine"]
   },
   {
-    img: "van.jpg",
+    img: "JPG/van.jpg",
     text: "Blorpoast Turbo 9000",
     subtitle: "Jellybean-wheeled toaster racer",
     choices: ["Desert of Sticky Doom", "Jam Canyon Rally"]
   },
   {
-    img: "juicebox.jpg",
+    img: "JPG/juicebox.jpg",
     text: "Juice Blaster XL",
     subtitle: "Hydrate an entire cartoon planet",
     choices: ["Drift into destiny", "High-five a gummy bear"]
   },
   {
-    img: "cone.jpg",
+    img: "JPG/cone.jpg",
     text: "Conezo Flipflip",
     subtitle: "Traffic cone breakdancer",
     choices: ["Drop the cheddar beat", "Challenge to a snackdance"]
   },
   {
-    img: "angry.jpg",
+    img: "JPG/angry.jpg",
     text: "Grumpalon the Rager",
     subtitle: "Screaming his confetti-covered truth",
     choices: ["Launch him anyway", "Join the tantrum"]
   },
   {
-    img: "duck.jpg",
+    img: "JPG/duck.jpg",
     text: "Undercover duck",
     subtitle: "Banana shield. Cereal city. Trust issues.",
     choices: ["Zoom banana", "Interrogate the jellybean cop"]
   },
   {
-    img: "ghost.jpg",
+    img: "JPG/ghost.jpg",
     text: "Ghost Toastington",
     subtitle: "Interrogate the guilty teacup",
     choices: ["Lick the jelly magnifier", "Lick the jelly magnifier"]
   },
   {
-    img: "snail.jpg",
+    img: "JPG/snail.jpg",
     text: "Snailio Blingzoom",
     subtitle: "cracker-limo cruisin’, leaving a trail of sparkle and swagger",
     choices: ["Lovely limo", "TOO CHEEESY"]
   },
   {
-    img: "poop.jpg",
+    img: "JPG/poop.jpg",
     text: "Captain Mcpoopy Creamy",
     subtitle: "ice-scream? YOU SCREAM",
     choices: ["Poopy rocket", "Love chocolate"]
   },
   {
-    img: "lamp.jpg",
+    img: "JPG/lamp.jpg",
     text: "Lampy Bloomcord",
     subtitle: "in a sky island of rainbow snails and bouncing blobs. ☁️",
     choices: ["Watch-out, cactus!", "Not too sharp"]
   },
   {
-    img: "mouse.jpg",
+    img: "JPG/mouse.jpg",
     text: "Mike McMoucy",
     subtitle: "cheese-gadget-loving, hoodie-rockin’, pizza-riding mouse legend",
     choices: ["Too cheesy", "LOVE cheese"]
   },
    {
-    img: "boat.jpg",
+    img: "JPG/boat.jpg",
     text: "McBoat McBoaty Face",
     subtitle: "100% floating on pure brain rot energy.",
     choices: ["Row, row, row the boat", "Jelly aint sea"]
   },
    {
-    img: "chicken.jpg",
+    img: "JPG/chicken.jpg",
     text: "ChiKEN",
     subtitle: "laundromat legend",
     choices: ["Do-nut watch TV!", "YOU CAN'T WASH DONUTS"]
   },
    {
-    img: "rock.jpg",
+    img: "JPG/rock.jpg",
     text: "Rockcell Signalblast",
     subtitle: "The mobile legend of the concert circuit 📱🎸",
     choices: ["We-will-rock YOU!", "Do you feel the connection?"]
